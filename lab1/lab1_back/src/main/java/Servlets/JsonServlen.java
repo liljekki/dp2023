b79128da26cb78513a2b3eef6aab8aa2c89c24b5
Kvartiuk <alexkvmit11@gmail.com>
2023-02-01 19:36:16 +0200

    back

--- lab1/lab1_back/src/main/java/Servlets/JsonServlen.java
+++ lab1/lab1_back/src/main/java/Servlets/JsonServlen.java
@@ -7,19 +7,27 @@ import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 
+import javax.swing.text.html.parser.Entity;
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.lang.reflect.Array;
+import java.util.ArrayList;
 
 @WebServlet("/servlet")
 public class JsonServlen extends HttpServlet {
     @Override
     public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
         MyEntity entity = new MyEntity("kraken", "good sleeping bag for hiking", "assets/img/spalnik.png");
-        String someJson = new Gson().toJson(entity);
+
+        ArrayList<MyEntity> data = new ArrayList<MyEntity>();
+        data.add(entity);
+
+        String mydata = new Gson().toJson(data);
+
         PrintWriter out = response.getWriter();
         response.setContentType("application/json");
         response.setCharacterEncoding("UTF-8");
-        out.println("[" + someJson + "]");
+        out.print(mydata);
         out.flush();
     }
 }
