57472a26b7176aa8a071e9ce60355fe85315f2e0
Kvartiuk <alexkvmit11@gmail.com>
2023-02-03 22:00:18 +0200

    front_lab1_edit

--- lab1/lab1_back/src/main/java/Servlets/JsonServlen.java
+++ lab1/lab1_back/src/main/java/Servlets/JsonServlen.java
@@ -7,10 +7,8 @@ import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 
-import javax.swing.text.html.parser.Entity;
 import java.io.IOException;
 import java.io.PrintWriter;
-import java.lang.reflect.Array;
 import java.util.ArrayList;
 
 @WebServlet("/servlet")
--- lab1/lab1_front/src/app/app.module.ts
+++ lab1/lab1_front/src/app/app.module.ts
@@ -6,14 +6,18 @@ import { AppComponent } from './app.component';
 import {HttpClientModule} from '@angular/common/http';
 import { NavbarComponent } from './components/navbar/navbar.component';
 import { ButtonComponent } from './components/button/button.component';
-import { AboutComponent } from './components/about/about.component'
+import { AboutComponent } from './components/about/about.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { HeaderComponent } from './components/header/header.component'
 
 @NgModule({
   declarations: [
     AppComponent,
     NavbarComponent,
     ButtonComponent,
-    AboutComponent
+    AboutComponent,
+    FooterComponent,
+    HeaderComponent
   ],
   imports: [
     BrowserModule,
