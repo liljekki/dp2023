103b01a940566292c624f8a1c84bddcdd7c106b3
Kvartiuk <alexkvmit11@gmail.com>
2023-03-04 16:14:31 +0200

    jpa

--- lab4/lab4_back/src/main/java/jdbc/app.java
+++ lab4/lab4_back/src/main/java/jdbc/app.java
@@ -1,5 +1,9 @@
 package jdbc;
-
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.boot.MetadataSources;
+import org.hibernate.boot.registry.StandardServiceRegistry;
+import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
 import java.sql.Connection;
 import java.sql.ResultSet;
 import java.sql.SQLException;
@@ -17,21 +21,55 @@ public class app {
 		LabCRUDInterface crud = new SqlCRUD();
 		Connection connection = new Connect().getCon();
 
-
+		List<MyEntity> entities;
 		List<MyEntity> list = new ArrayList<>();
 
+
+//JDBC connection
 		try (Statement st = connection.createStatement(); ResultSet rs = st.executeQuery("SELECT * FROM entity.entity;");) {
 			while (rs.next()) {
 				list.add(new MyEntity(rs.getInt(1), rs.getString(2), rs.getString(3), rs.getString(4)));
 			}
-
 		} catch (SQLException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
 
-		System.out.println(list);
+		System.out.println("JDBC");
+		for (MyEntity entity : list) {
+			System.out.println(entity);
+		}
 
+
+//JPA connection
+		final StandardServiceRegistry registry = new StandardServiceRegistryBuilder()
+				.configure() // configures settings from hibernate.cfg.xml
+				.build();
+		try (SessionFactory sessionFactory = new MetadataSources(registry)
+				.addAnnotatedClass(MyEntity.class)
+				.buildMetadata()
+				.buildSessionFactory()) {
+
+			Session session = sessionFactory.openSession();
+			session.beginTransaction();
+
+            session.save(new MyEntity(3, "name3", "gg", "img_new"));
+            session.save(new MyEntity(2, "name2", "5","1"));
+
+            MyEntity updateEntity1 = new MyEntity(1, "updated_name_1", "update", "gg");
+            session.update(updateEntity1);
+            session.delete(new MyEntity(2, "name2", "5","1"));
+
+			entities = (List<MyEntity>) session.createQuery("from MyEntity").list();
+
+			session.getTransaction().commit();
+
+		}
+
+		System.out.println("JPA");
+		for (MyEntity entity : entities) {
+			System.out.println(entity);
+		}
+		System.out.println("Success!");
 	}
-
 }
